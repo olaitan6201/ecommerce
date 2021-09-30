c880458629d0a3e7900e73d7c65ddc1383450ded
Olaitan Habeeb Olamide <olaitan6201@gmail.com>
2021-09-30 10:20:46 +0100

    Edited migration files and run migration

--- database/migrations/2021_09_30_090411_create_products_table.php
+++ database/migrations/2021_09_30_090411_create_products_table.php
@@ -15,7 +15,21 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100)->nullable();
+            $table->string('slug', 100)->nullable()->unique();
+            $table->string('short_descripion', 100)->nullable();
+            $table->text('descripion')->nullable();
+            $table->decimal('regular_price', 5, 2)->nullable();
+            $table->decimal('sale_price', 5, 2)->nullable();
+            $table->string('SKU', 100)->nullable();
+            $table->enum('stock_status', ['instock', 'outofstock'])->nullable();
+            $table->boolean('featured')->default(false);
+            $table->unsignedInteger('quantity')->default(10);
+            $table->string('image')->nullable();
+            $table->text('images')->nullable();
+            $table->bigInteger('category_id')->unsigned()->nullable();
             $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
